2818b7083d2e0815d6167be369d9294656d35c81
Yurii Kobets <yuriikobets@gmail.com>
2022-08-04 20:46:38 +0200

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Header() {
+    return (
+        <header>
+            <h1>Test text to display hgvhfhgjkhkhgugyu</h1>
+        </header>
+    );
+}
